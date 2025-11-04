07567697d1d5d0000690310bd5faf3ad5b6c232b
Naga Vinay Avvaru <nagavinay0905@gmail.com>
2025-11-04 14:59:48 +0530

    Add solution for longest consecutive sequence problem
    Implement solution to find longest consecutive sequence in an array.

---
+++ 128. Longest Consecutive Sequence.java
@@ -0,0 +1,55 @@
+// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+// You must write an algorithm that runs in O(n) time.
+
+ 
+
+// Example 1:
+
+// Input: nums = [100,4,200,1,3,2]
+// Output: 4
+// Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+// Example 2:
+
+// Input: nums = [0,3,7,2,5,8,4,6,0,1]
+// Output: 9
+// Example 3:
+
+// Input: nums = [1,0,1,2]
+// Output: 3
+ 
+
+// Constraints:
+
+// 0 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        int n=nums.length;
+        if(n==0){
+            return 0;
+
+        }
+
+        int longest=1;
+        Set<Integer> set=new HashSet<>();
+        for(int i =0;i<n;i++){
+            set.add(nums[i]);
+        }
+        for(int it: set){
+            if(!set.contains(it-1)){
+                int cnt=1;
+                int x=it;
+
+                while(set.contains(x+1)){
+                    x=x+1;
+                    cnt=cnt+1;
+                }
+                longest=Math.max(longest,cnt);
+            }
+        }
+        return longest;
+    }
+}
+
