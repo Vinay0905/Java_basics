a4083440941ee6d60b524cc2ec02025205218fc3
Naga Vinay Avvaru <nagavinay0905@gmail.com>
2025-11-04 15:22:29 +0530

    Create 73. Set Matrix Zeroes.java

---
+++ 73. Set Matrix Zeroes.java
@@ -0,0 +1,68 @@
+// Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+// You must do it in place.
+
+ 
+
+// Example 1:
+
+
+// Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+// Output: [[1,0,1],[0,0,0],[1,0,1]]
+// Example 2:
+
+
+// Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+// Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ 
+
+// Constraints:
+
+// m == matrix.length
+// n == matrix[0].length
+// 1 <= m, n <= 200
+// -231 <= matrix[i][j] <= 231 - 1
+
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int col0 = 1;
+
+        // Step 1: Traverse the matrix and mark first row and column accordingly:
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][0] == 0) col0 = 0;  // Check first column separately
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;  // mark i-th row
+                    matrix[0][j] = 0;  // mark j-th column
+                }
+            }
+        }
+
+        // Step 2: Use marks to set zeros inside the matrix (excluding first row and col)
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Step 3: Finally set zeros for first row if needed
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Step 4: Finally set zeros for first column if needed
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
+
