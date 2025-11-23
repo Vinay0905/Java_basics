339c3ddcdb4be18eef4bd5205b909de30fccc08e
Naga Vinay Avvaru <nagavinay0905@gmail.com>
2025-11-23 10:49:33 +0530

    Create 241. Different Ways to Add Parentheses.cpp

---
+++ 241. Different Ways to Add Parentheses.cpp
@@ -0,0 +1,55 @@
+// Given a string expression of numbers and operators, return all possible results from computing all the different possible ways to group numbers and operators. You may return the answer in any order.
+
+// The test cases are generated such that the output values fit in a 32-bit integer and the number of different results does not exceed 104.
+
+ 
+
+// Example 1:
+
+// Input: expression = "2-1-1"
+// Output: [0,2]
+// Explanation:
+// ((2-1)-1) = 0 
+// (2-(1-1)) = 2
+// Example 2:
+
+// Input: expression = "2*3-4*5"
+// Output: [-34,-14,-10,-10,10]
+// Explanation:
+// (2*(3-(4*5))) = -34 
+// ((2*3)-(4*5)) = -14 
+// ((2*(3-4))*5) = -10 
+// (2*((3-4)*5)) = -10 
+// (((2*3)-4)*5) = 10
+ 
+
+// Constraints:
+
+// 1 <= expression.length <= 20
+// expression consists of digits and the operator '+', '-', and '*'.
+// All the integer values in the input expression are in the range [0, 99].
+// The integer values in the input expression do not have a leading '-' or '+' denoting the sign.
+
+class Solution {
+public:
+    vector<int> diffWaysToCompute(string expression) {
+        vector<int> res;
+        for (int i = 0; i < expression.size(); ++i) {
+            char oper = expression[i];
+            if (oper == '+' || oper == '-' || oper == '*') {
+                vector<int> s1 = diffWaysToCompute(expression.substr(0, i));
+                vector<int> s2 = diffWaysToCompute(expression.substr(i + 1));
+                for (int a : s1) {
+                    for (int b : s2) {
+                        if (oper == '+') res.push_back(a + b);
+                        else if (oper == '-') res.push_back(a - b);
+                        else if (oper == '*') res.push_back(a * b);
+                    }
+                }
+            }
+        }
+        if (res.empty()) res.push_back(stoi(expression));
+        return res;
+    }
+};
+
